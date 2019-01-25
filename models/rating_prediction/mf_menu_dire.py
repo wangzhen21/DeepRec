bb56ef882dfea44f3178f34671dd17933cf32901
wangzhen21 <wangzhenqinghua@163.com>
2019-01-25 16:21:18 +0800

    m

--- models/rating_prediction/mf_menu_dire.py
+++ models/rating_prediction/mf_menu_dire.py
@@ -12,7 +12,7 @@ import sys
 
 from utils.evaluation.RatingMetrics import *
 class MF_manu_dire_neg_pos():
-    def __init__(self, sess, num_user, num_item, num_dire,learning_rate=0.001, reg_rate=0.01, epoch=500, batch_size=128,
+    def __init__(self, sess, num_user, num_item, num_dire,learning_rate=0.001, reg_rate=0.008, epoch=500, batch_size=256,
                  show_time=False, T=1, display_step=1000):
         self.learning_rate = learning_rate
         self.epochs = epoch
